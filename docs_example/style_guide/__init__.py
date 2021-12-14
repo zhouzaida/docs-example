0467edc46dfce2a0700086cba1088dc491aae862
zhouzaida <zhouzaida@163.com>
2021-12-14 15:51:09 +0800

    Add module in __init__.py

--- docs_example/style_guide/__init__.py
+++ docs_example/style_guide/__init__.py
@@ -1,3 +1,7 @@
-from .docstring import ClassDocstring
+from .docstring import (ExampleClass, ExampleClass2, ExampleError,
+                        example_generator, module_level_function)
 
-__all__ = ['ClassDocstring']
+__all__ = [
+    'module_level_function', 'example_generator', 'ExampleError',
+    'ExampleClass', 'ExampleClass2'
+]
